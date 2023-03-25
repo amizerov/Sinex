d31a26b7e641b771eb4dbc9c8c701cc80d5c76e8
Andrey Mizerov <andrey@mizerov.com>
2023-03-25 17:20:28 +0300

    refac 2

--- caLibProdStat/Exchange/Binance.cs
+++ caLibProdStat/Exchange/Binance.cs
@@ -17,10 +17,10 @@ public class Binance : AnExchange
         BinanceProduct binaProd = (BinanceProduct)p;
 
         Product product = new();
+        product.symbol = binaProd.Symbol;
         product.exchange = ID;
-        product.symbol = binaProd.Symbol.ToUpper().Replace("-", "");
-        product.baseasset = binaProd.BaseAsset.ToUpper();
-        product.quoteasset = binaProd.QuoteAsset.ToUpper();
+        product.baseasset = binaProd.BaseAsset;
+        product.quoteasset = binaProd.QuoteAsset;
 
         product.IsTradingEnabled = binaProd.Status == "TRADING";
 
