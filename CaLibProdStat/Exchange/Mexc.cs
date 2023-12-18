07c2285ad6b83f0d12c3d60274f9115c1e236d39
Andrey Mizerov <andrey@mizerov.com>
2023-12-18 19:36:15 +0300

    Mexc 1

---
+++ CaLibProdStat/Exchange/Mexc.cs
@@ -0,0 +1,25 @@
+﻿using CryptoExchange.Net.CommonObjects;
+
+namespace caLibProdStat;
+
+public class Mexc : AnExchange
+{
+    public override int ID => 10;
+
+    public override string Name => "Mexc";
+
+    protected override List<Kline> GetLastKlines(string symbol)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override List<Product> GetProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Product ToProduct(object p)
+    {
+        throw new NotImplementedException();
+    }
+}
